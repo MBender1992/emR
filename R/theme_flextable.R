e314c6408767a8891de4201031cc446ecc74ca51
MBender1992 <mbender1992@web.de>
2023-09-20 10:49:13 +0200

    Update theme_flextable.R
    - change padding of tables to 0.2

--- R/theme_flextable.R
+++ R/theme_flextable.R
@@ -8,7 +8,7 @@
 theme_flextable <- function(ft, textsize = 9, headsize = 10){
   tmp <- flextable::bold(ft, bold = TRUE, part = "header")
   tmp <- fontsize(tmp,  size = textsize, part = "body")
-  tmp <- padding(tmp, padding = 0, part = "all")
+  tmp <- padding(tmp, padding = 0.2, part = "all")
   tmp <- line_spacing(tmp, space = 1, part = "all")
   out <- fontsize(tmp,  size = headsize, part = "header")
   return(out)
